b9752d5304df8fa95db665eac7c8841c97a49bcf
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2024-04-16 10:54:24 -0300

    feat: Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-strongly-recommended',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    semi: ['error', 'never'],
+    quotes: ['error', 'single'],
+    indent: ['error', 2],
+    'comma-spacing': ['error', { before: false, after: true }]
+  }
+}
