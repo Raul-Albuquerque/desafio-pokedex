20147a145bfa79b1f5dc4bb81cb959f639352c5a
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2024-04-16 16:05:52 -0300

    feat: added language switcher

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,43 @@
 <script setup>
+import { reactive } from 'vue'
 
-import Header from './components/HeaderSection.vue'
+import Header from './components/Header.vue'
+import CardList from './components/CardList.vue'
+
+const state = reactive({
+  language: 'pt'
+})
+
+const setLanguage = (language) => {
+  return state.language = language
+}
 
 </script>
 
 <template>
-  <Header />
+  <Header>
+    <div class="d-flex align-items-center justify-content-end">
+      <button
+        class="btn btn-outline-secondary ms-2"
+        @click="setLanguage('en')"
+      >
+        <img src="/images/en.svg">
+        EN
+      </button>
+      <button
+        class="btn btn-outline-secondary ms-2"
+        @click="setLanguage('pt')"
+      >
+        <img src="/images/pt.svg">
+        PT-BR
+      </button>
+      <button
+        class="btn btn-outline-secondary ms-2"
+        @click="setLanguage('es')"
+      >
+        <img src="/images/es.svg">
+        ES
+      </button>
+    </div>
+  </Header>
 </template>
-
-<style scoped>
-
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="bg-secondary bg-opacity-25">
+    <div class="container p-2 d-flex align-items-center justify-content-between">
+      <h1>
+        <img
+          src="/images/logo.svg"
+          alt="logo pokédex"
+        >
+      </h1>
+      <slot />
+    </div>
+  </header>
+</template>
